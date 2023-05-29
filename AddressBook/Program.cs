38c5c250f5c841662a7b5b640bc889583824d683
Shri10 <pethe.shriraj@gmail.com>
2023-05-29 09:16:00 +0530

    [Shri10] UC1 Created

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,6 +5,34 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Address Book Program!");
+
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            string lastName = Console.ReadLine();
+
+            Console.Write("Enter Address: ");
+            string address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            string city = Console.ReadLine();
+
+            Console.Write("Enter State: ");
+            string state = Console.ReadLine();
+
+            Console.Write("Enter Zip Code: ");
+            string zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            string phoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email: ");
+            string email = Console.ReadLine();
+
+            Contacts contact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+            Console.WriteLine($"Created contact: {contact.FirstName} {contact.LastName}");
         }
     }
 }
