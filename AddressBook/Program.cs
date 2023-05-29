99cbaddd329c1902d1dc9cdc19cdd06a7770a789
Shri10 <pethe.shriraj@gmail.com>
2023-05-29 09:41:03 +0530

    [Shri10] Created UC

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -37,6 +37,51 @@
             addressBook.AddContact(contact);
 
             Console.WriteLine($"Added contact: {contact.FirstName} {contact.LastName}");
+
+            Console.WriteLine("Do you want to edit a contact? (yes/no)");
+            string editInput = Console.ReadLine();
+
+            if (editInput.ToLower() == "yes")
+            {
+                Console.Write("Enter First Name of contact to edit: ");
+                string firstNameToEdit = Console.ReadLine();
+
+                Console.Write("Enter Last Name of contact to edit: ");
+                string lastNameToEdit = Console.ReadLine();
+
+                Console.WriteLine("Enter new contact details:");
+
+                Console.Write("Enter First Name: ");
+                firstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name: ");
+                lastName = Console.ReadLine();
+
+                Console.Write("Enter Address: ");
+                address = Console.ReadLine();
+
+                Console.Write("Enter City: ");
+                city = Console.ReadLine();
+
+                Console.Write("Enter State: ");
+                state = Console.ReadLine();
+
+                Console.Write("Enter Zip Code: ");
+                zip = Console.ReadLine();
+
+                Console.Write("Enter Phone Number: ");
+                phoneNumber = Console.ReadLine();
+
+                Console.Write("Enter Email: ");
+                email = Console.ReadLine();
+
+                Contacts newContact = new Contacts(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+                addressBook.EditContact(firstNameToEdit, lastNameToEdit, newContact);
+
+                Console.WriteLine($"Edited contact: {newContact.FirstName} {newContact.LastName}");
+            }
+
         }
     }
 }
